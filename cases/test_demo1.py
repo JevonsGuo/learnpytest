75a969b8e4b4241f332f2fa4ee6cce8e2108e71f
Jevons_Guo <woainijevons@msn.com>
2021-05-13 15:35:36 +0800

    1st demo1

---
+++ cases/test_demo1.py
@@ -0,0 +1,13 @@
+class Test_demo:
+
+    def test_c001(self):
+        print('\n用例c001')
+        assert 1 == 1
+
+    def test_c002(self):
+        print('\n用例c002')
+        assert 2 == 2
+
+    def test_c003(self):
+        print('\n用例c003')
+        assert 3 == 3
